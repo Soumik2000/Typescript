4051c32e7450ccda394ca926102e2b98e2eef696
Soumik <soumik.chandra2000@gmail.com>
2023-04-26 15:09:02 +0530

    Adding a typescript and transcompiled Javascript file that demonstrate search operation.

---
+++ searchElement.js
@@ -0,0 +1,7 @@
+var input = ["electronics", "food", "clothes"];
+if (input.includes("food")) {
+    console.log("Present");
+}
+else {
+    console.log("Not present");
+}
---
+++ searchElement.ts
@@ -0,0 +1,8 @@
+var input:Array<string> = ["electronics","food","clothes"];
+
+if(input.includes("food")){
+    console.log("Present");
+}
+else{
+    console.log("Not present");
+}
