3b4608ed5ef8e36448357657d1053c596e2a0941
Soumik <soumik.chandra2000@gmail.com>
2023-04-26 15:07:04 +0530

    Adding a file that demonstrates type comparision of variables.

---
+++ comparingTypes.js
@@ -0,0 +1,4 @@
+var a = 10;
+var b = "Soumik";
+console.log(typeof a == "number");
+console.log(typeof b == "string");
---
+++ comparingTypes.ts
@@ -0,0 +1,5 @@
+var a:number = 10;
+var b:string = "Soumik";
+
+console.log(typeof a == "number");
+console.log(typeof b == "string");
